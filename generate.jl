3c3a99a66f58d6e3439b2a6d117eec6e32f766d6
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-23 00:57:57 +0900

    update

--- generate.jl
+++ generate.jl
@@ -1,6 +1,36 @@
-using PkgTemplates
+using ArgParse
 
+using PkgTemplates
+import PkgTemplates: Template
+using PkgTemplates: priority, prehook, hook, posthook
 include("myplugins.jl")
+
+t_with_jupyter = Template(;
+    dir = pwd(),
+    julia = v"1",
+    plugins = [
+        License(; name = "MIT"),
+        Git(; manifest = false, ssh = true),
+        GitHubActions(;
+            extra_versions = ["1.0", "1.6", "nightly"]
+        ),
+        Documenter{GitHubActions}(),
+        Readme(;
+            inline_badges = true,
+            badge_order = DataType[
+                GitHubActions,
+                Documenter{GitHubActions},
+            ]
+        ),
+        Dockerfile(with_jupyter = true),
+        DockerCompose(),
+        Makefile(),
+        DevContainer(),
+        Jupytext(),
+        PlaygroundNotebook(),
+        PlaygroundPluto(),
+    ]
+)
 
 t = Template(;
     dir = pwd(),
@@ -19,14 +49,36 @@ t = Template(;
                 Documenter{GitHubActions},
             ]
         ),
-        Dockerfile(),
-        DockerCompose(),
+        Dockerfile(with_jupyter = false),
+        DockerCompose(with_jupyter=false),
         Makefile(),
         DevContainer(),
-        Jupytext(),
-        DocsPatchFile(),
-        PlaygroundNotebook(),
-        PlaygroundPluto(),
     ]
 )
 
+function main()
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "pkgname"
+            help = "Pkg name"
+            required = true
+        "--with-jupyter"
+            help = "create template for Jupyter workflow"
+            action = :store_true
+    end
+    parsed_args = parse_args(ARGS, s)
+    pkgname = parsed_args["pkgname"]
+    if !endswith(pkgname, ".jl")
+        pkgname *= ".jl"
+    end
+    if parsed_args["with-jupyter"]
+        t_jupyter(pkgname)
+    else
+        t(pkgname)
+    end
+    nojl = split(pkgname, ".jl")[begin]
+    @info "Rename $nojl => $pkgname"
+    mv(nojl, pkgname)
+end
+
+main()
--- myplugins.jl
+++ myplugins.jl
@@ -2,29 +2,27 @@ using PkgTemplates
 using PkgTemplates: FilePlugin, Template
 import PkgTemplates: source, destination, view
 
-Base.@kwdef struct Dockerfile <: FilePlugin
-    file::String = "templates/Dockerfile"
-    destination::String = "Dockerfile"
+struct Dockerfile <: FilePlugin
+    file::String
+    destination::String
+    function Dockerfile(;with_jupyter=false)
+        if with_jupyter
+            new(joinpath("templates", "with_jupyter", "Dockerfile"), "Dockerfile")
+        else
+            new(joinpath("templates", "Dockerfile"), "Dockerfile")
+        end
+    end
 end
 
 source(p::Dockerfile) = p.file
 destination(p::Dockerfile) = p.destination
+view(::Dockerfile, ::Template, pkg::AbstractString) = Dict("PKG" => pkg)
 
 # ---
 
 Base.@kwdef struct Jupytext <: FilePlugin
     file::String = "templates/jupytext.toml"
     destination::String = "jupytext.toml"
-end
-
-source(p::Jupytext) = p.file
-destination(p::Jupytext) = p.destination
-
-# ---
-
-Base.@kwdef struct DocsPatchFile <: FilePlugin
-    file::String = "templates/patch4liveserver.jl"
-    destination::String = "docs/patch4liveserver.jl"
 end
 
 source(p::Jupytext) = p.file
@@ -72,9 +70,16 @@ destination(p::DevContainer) = p.destination
 
 # ---
 
-Base.@kwdef struct DockerCompose <: FilePlugin
-    file::String = "templates/docker-compose.yml"
-    destination::String = "docker-compose.yml"
+struct DockerCompose <: FilePlugin
+    file::String
+    destination::String
+    function DockerCompose(;with_jupyter=false)
+        if with_jupyter
+            new(joinpath("templates", "with_jupyter", "docker-compose.yml"), "docker-compose.yml")
+        else
+            new(joinpath("templates", "docker-compose.yml"), "docker-compose.yml")
+        end
+    end
 end
 
 source(p::DockerCompose) = p.file
